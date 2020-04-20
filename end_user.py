f3c5bc7a85fd669587f5a7aa1c47e5ebcf2b3ae9
Ananya <anunna@ncsu.edu>
2020-04-20 17:40:23 -0400

    end user update

--- end_user.py
+++ end_user.py
@@ -1,20 +1,23 @@
 import sys
 import os
 import socket
-PORT=52535
+PORT=52526
 ip="192.168.0.22" # closes edge server IP that is hard coded
 while True:
     x=input("Hi!  WHAT FILE WOULD YOU LIKE TO ACCESS?")
-    path=input("Where do you want it to be saved?")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip,PORT))
-    s.sendall(x+".txt")
+    print("Going to send request")
+    s.send((x+".txt").encode())
+    print("Sent request")
+    print("Waiting for file to be sent")
     l=s.recv(1024)
     f=open("./Downloads/"+x+".txt","w")
     while (l):
-        l=s.recv(1024)
+        l=s.recv(1024).decode()
         f.write(l)
-        f.close() 
+        print("writing the file...")
+    f.close() 
     y=input("YOU HAVE RECEIVED THE FILE. \n Continue for more? (y/n)")
     if y =='n':
         break
