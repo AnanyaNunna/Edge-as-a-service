48423446ee30973e9b7df1e659e7da3a8f332eca
Ananya <anunna@ncsu.edu>
2020-04-21 18:40:27 -0400

    almost working test case 3

--- edge.py
+++ edge.py
@@ -33,7 +33,7 @@ class Threads:
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.bind((HOST, PORT))
-        self.c.bind((HOST, PORT_C))
+        #self.c.bind((HOST, PORT_C))
 
     def get_cache(self):
         cache_list = []
@@ -50,9 +50,11 @@ class Threads:
             f.write("\n")
         f.close()
 
-    def send(self,file_to_send,conn1):
+    def send(self,file_to_send,conn1,flag):
         f = open("./files/"+file_to_send,'r')
         print("Sending file now ....")
+        if flag==2:
+            conn1.send(("Hey there, this is "+HOST).encode())
         l=f.read()
         l_len = len(l)
         conn1.send(l_len.to_bytes(4,'big'))
@@ -66,6 +68,7 @@ class Threads:
         for i in dat:
             line = i.split(":")
             key = line[0]
+            line[1]=line[1].strip("\n")
             edge_list = line[1].split(",")
             d[key] = edge_list
         return d
@@ -78,21 +81,22 @@ class Threads:
                 f.write(i+",")
             f.write(v[-1]+"\n")
 
-    def client(self, ip_or_flag , file_name):
+    def client(self, ip_or_flag , file_name,port):
         edge_stat_dict = self.convert()
         if ip_or_flag == "broadcast":
             for ip in edge_stat_dict.keys():
                 if(ip != HOST):
-                    self.c.connect((ip,PORT_C))
+                    self.c.connect((ip,port))
                     self.c.send("edge stats incomming".encode())
                     f1=open("./edgestat.txt","r")
                     l=f1.read()
                     l_len = len(l)
                     self.c.send(l_len.to_bytes(4,'big'))
                     self.c.send(l.encode())
+                    self.c.shutdown(1)
 
         else:
-            self.c.connect((ip_or_flag,PORT_C))
+            self.c.connect((ip_or_flag,port))
             self.c.send(file_name.encode())
             #print("sent file name to origin")
             msg=self.c.recv(1024).decode()
@@ -108,6 +112,7 @@ class Threads:
                 l_len-=len(l)
                 g.write(l)
             g.close()
+            self.c.shutdown(0)
             print("Written to the file completely")
             print("file is received from origin")
 
@@ -136,7 +141,7 @@ class Threads:
                 print(cache_list)
                 if req_msg.split(".")[0] in cache_list:
                     print("entered .txt loop")
-                    self.send(req_msg,conn)
+                    self.send(req_msg,conn,2)
                     print("sent .txt")
                     cache_list.remove(req_msg.split(".")[0])
                     cache_list.insert(0,req_msg.split(".")[0])
@@ -146,19 +151,20 @@ class Threads:
                 else:
                     cache_list = self.get_cache()
                     edge_stat_dict = self.convert()
+                    print(edge_stat_dict)
                     call_origin = True
                     for i in pr:
                         if req_msg.split(".")[0] in edge_stat_dict[i]:
                             print("call client with the edge IP")
                             call_origin = False
-                            self.client(i,req_msg)
+                            self.client(i,req_msg,PORT)
                             break
 
                     if call_origin:
                         print("Calling origin")
-                        self.client(ORIGINIP,req_msg)
+                        self.client(ORIGINIP,req_msg,PORT_C)
                         
-                    self.send(req_msg, conn)
+                    self.send(req_msg, conn,1)
                     if len(cache_list) > 9:
                             cache_list.pop()
                     cache_list.insert(0,req_msg.split(".")[0])
@@ -166,7 +172,7 @@ class Threads:
                     edge_stat_dict[HOST]=cache_list
                     print(edge_stat_dict)
                     self.update_edge(edge_stat_dict)
-                    self.client("broadcast",req_msg)
+                    self.client("broadcast",req_msg,PORT)
 
             elif "edge" in req_msg:
                 f=open("./edgestat.txt","w")
